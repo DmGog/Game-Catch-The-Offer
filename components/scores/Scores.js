1ae56cea0ec1e8e5c241631008e39b044a80f892
Dmitriy <goglev37@gmail.com>
2024-03-11 19:34:56 +0300

    add style

--- components/scores/Scores.js
+++ components/scores/Scores.js
@@ -2,6 +2,7 @@ import {data} from "../../data/data.js";
 
 export function Scores() {
     const container = document.createElement("div");
-    container.append(`Catch: ${data.scores.catchesCount}; Miss: ${data.scores.missesCount}`);
+    container.className = "scores"
+    container.append(`Catch: ${data.scores.catchesCount}  Miss: ${data.scores.missesCount}`);
     return container
 }
